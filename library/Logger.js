3a5d43828f9076d6c500d012035c7c30eeb59b65
Silent <eithantuit10@gmail.com>
2019-05-27 10:42:27 +1000

    file rotation

--- library/Logger.js
+++ library/Logger.js
@@ -1,25 +1,28 @@
 const winston = require('winston');
+const winstonFileRotator = require('winston-daily-rotate-file');
 const path = require('path');
 const logdir = path.join(__dirname, '../logs');
 const argument = require('./argument');
 
-const filenamePrefix = argument.get('id') || 1;
+const filenamePrefix = argument.get('id') || 'default';
 
 module.exports = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({
-			filename: path.join(logdir, `${filenamePrefix}-error.log`),
-			level: 'error'
+    new winstonFileRotator({
+			filename: `${filenamePrefix}-%DATE%-error.log`,
+			dirname: logdir,
+			level: 'error',
+			zippedArchive: true,
+			maxFiles: 32,
 		}),
 
-    new winston.transports.File({
-			filename: path.join(logdir, `${filenamePrefix}-log.log`),
-		}),
-
-		new winston.transports.Console({
-			format: winston.format.simple(),
+    new winstonFileRotator({
+			filename: `${filenamePrefix}-%DATE%-log.log`,
+			dirname: logdir,
+			zippedArchive: true,
+			maxFiles: 32,
 		})
-  ]
+	]
 });
